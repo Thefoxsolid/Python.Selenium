28b5c991a88993ac0258ef6ee523498055a833fc
Thefoxsolid <francisco.martinezoyaneder@gmail.com>
2022-02-02 13:44:31 -0300

    Codigo Actualizado

--- BuscadorChrome.py
+++ BuscadorChrome.py
@@ -1,26 +1,37 @@
 # Importar Librerías    
 
 from lib2to3.pgen2 import driver
-import unittest
+from ssl import Options
+from telnetlib import EC
 from webbrowser import Chrome
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
 import time
+import pandas as pd
+
+# Opciones de navegación
+
+Options = webdriver.ChromeOptions()
+Options.add_argument('--star-maximized')
+Options.add_argument('--disable-extensions')
+
 
 Chrome_driver =  webdriver.Chrome("C:\\Users\FRANCISCO\Downloads\chromedriver.exe")
+
+# Iniciador del Navegador
+
 Chrome_driver.get("https://www.solotodo.cl/")
 
 Chrome_driver.get("https://www.solotodo.cl/cells")
 
-print(driver.title)
 
-search_bar = driver.find_element_by_name("cells")
+# Buscar elementos navegador 
 
-# lista de productos
-
-celular = driver.find_element_by_class_name("class= css-16pqwjk-indicatorContainer")
-
-celular = driver.find_element_by_class_name("class= css-12jo7m5")
+WebDriverWait(driver,5)\
+    .until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'search#inputSearch'))).send_keys('Apple')
+    
 
 
 
