8af080098db90ce0246c7a6d8f8d4b76f791b3bc
Thefoxsolid <francisco.martinezoyaneder@gmail.com>
2022-02-15 10:55:52 -0300

    Actualizacion codigo test1

--- Test 1.py
+++ Test 1.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 import random
+import pandas as pd
 
 # Ruta de  navegación
 
@@ -24,5 +25,37 @@ buscador_ventana = Chrome_driver.find_element_by_xpath('//*[@id="navbar"]/div/ul
 buscador_ventana.click() 
 
 
+equipos = Chrome_driver.find_element_by_tag_name('products')
+
+for equipo in equipos:
+ print(equipos.txt)
 
 
+
+
+
+
+
+
+
+
+
+
+
+#buscador_list = []
+
+ #for producto in Chrome_driver:
+    
+    #title = Chrome_driver.find_element_by_link_text('.//*[@id="filters-and-results"]/div/div/div[1]/div[2]/div/div[1]/h3/a').text
+    #store = Chrome_driver.find_element_by_xpath('.//*[@id="filters-and-results"]/div/div/div[1]/div[2]/div/div[1]/div[2]/dl/dd[3]').text
+    #value = Chrome_driver.find_element_by_xpath('.//*[@id="filters-and-results"]/div/div/div[1]/div[2]/div/div[1]/div[3]/div/a').text
+    
+    #product_item = {
+       # 'title': title,
+        #'store': store,
+        #'value': value
+        #}
+
+    #buscador_list.append(product_item)
+    #df = pd.DataFrame(buscador_list)
+    #print(df)
