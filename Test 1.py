7aba4f6a917ecf455748aad2edc0504b13b9371c
Thefoxsolid <francisco.martinezoyaneder@gmail.com>
2022-02-15 17:17:16 -0300

    codigo realizando scrappaing

--- Test 1.py
+++ Test 1.py
@@ -24,38 +24,20 @@ buscador_ventana.send_keys('Apple iphone')
 buscador_ventana = Chrome_driver.find_element_by_xpath('//*[@id="navbar"]/div/ul[2]/div/form/div/div/div/button/i')
 buscador_ventana.click() 
 
+buscador_list = []
 
-equipos = Chrome_driver.find_element_by_tag_name('products')
-
-for equipo in equipos:
- print(equipos.txt)
-
-
-
-
-
-
-
-
-
-
-
-
-
-#buscador_list = []
-
- #for producto in Chrome_driver:
+for product in Chrome_driver:
     
-    #title = Chrome_driver.find_element_by_link_text('.//*[@id="filters-and-results"]/div/div/div[1]/div[2]/div/div[1]/h3/a').text
-    #store = Chrome_driver.find_element_by_xpath('.//*[@id="filters-and-results"]/div/div/div[1]/div[2]/div/div[1]/div[2]/dl/dd[3]').text
-    #value = Chrome_driver.find_element_by_xpath('.//*[@id="filters-and-results"]/div/div/div[1]/div[2]/div/div[1]/div[3]/div/a').text
+    title = Chrome_driver.find_element_by_link_text('//*[@id="filters-and-results"]/div/div/div[1]/div[2]/div/div[1]/h3/a').text
+    store = Chrome_driver.find_element_by_xpath('//*[@id="filters-and-results"]/div/div/div[1]/div[2]/div/div[1]/div[2]/dl/dd[3]').text
+    value = Chrome_driver.find_element_by_xpath('//*[@id="filters-and-results"]/div/div/div[1]/div[2]/div/div[1]/div[3]/div/a').text
     
-    #product_item = {
-       # 'title': title,
-        #'store': store,
-        #'value': value
-        #}
+    product_item = {
+        'title': title,
+        'store': store,
+        'value': value
+        }
 
-    #buscador_list.append(product_item)
-    #df = pd.DataFrame(buscador_list)
-    #print(df)
+    buscador_list.append(product_item)
+    df = pd.DataFrame(buscador_list)
+    print(product_item.txt)
